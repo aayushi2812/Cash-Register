831acf76323d1a9d74a7012757a27c3fba803c28
Aayushi Parekh <aayushiparekh2812@gmail.com>
2024-12-23 11:16:41 -0500

    Commiting modified gradle files

--- src/main/java/com/example/cashregister/ProductsServiceClass.java
+++ src/main/java/com/example/cashregister/ProductsServiceClass.java
@@ -7,7 +7,7 @@ public class ProductsServiceClass {
     ArrayList<Products> productsList = new ArrayList<>(0);
 
     ProductsServiceClass(){
-        productsList.add(new Products("Pants","20","10"));
+        productsList.add(new Products("Pants","20","20"));
         productsList.add(new Products("Shirts","10","20"));
     }
     void addNewProduct(Products product){
